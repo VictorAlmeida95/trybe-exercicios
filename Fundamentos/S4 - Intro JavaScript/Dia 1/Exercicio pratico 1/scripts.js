1f7d83d72dcfa1670ad7c57a9cb088a6bc9a4c19
Victor <vitoralmeida95@hotmail.com>
2022-09-21 15:02:33 -0300

    exercicios 4.1 pt1

---
+++ Fundamentos/S4 - Intro JavaScript/Dia 1/Exercicio pratico 1/scripts.js
@@ -0,0 +1,16 @@
+
+// const myName = 'Victor';
+// const birthCity = 'São Paulo';
+// let birthYear = 2000;
+
+
+// console.log('Meu nome é '+ myName +' Nasci em ' + birthCity +' nos anos '+ birthYear);
+
+// 
+
+let birthYear = 2000;
+birthYear = 2030;
+
+console.log(birthYear);
+
+// não é possivel reatribuir uma constante.
---
+++ Fundamentos/S4 - Intro JavaScript/Dia 1/Exercicio pratico 3/scripts.js
@@ -0,0 +1,5 @@
+const notaTec = 80;
+
+if (notaTec >= 80) {console.log('Parabéns, você foi aprovada(o)!')}
+else if (notaTec >= 60 && notaTec < 80) {console.log('Você está na nossa lista de espera.')}
+else {console.log('Você foi reprovada(o).')};
