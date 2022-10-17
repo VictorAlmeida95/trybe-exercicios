c32d2104e2c3e2a9bc69f9844dd31f1971823aeb
Victor <vitoralmeida95@hotmail.com>
2022-10-17 16:39:03 -0300

    exercicio 1 completo

---
+++ Fundamentos/S7 - Intro ES6 e Testes unitários/Dia 1/Exercicio 2/script.js
@@ -0,0 +1,8 @@
+const oddsAndEvens = [13, 3, 4, 10, 7, 2];
+
+const sortOddsAndEvens = () => {
+
+    `Os números ${oddsAndEvens} se encontram ordenados de forma crescente!`
+
+}
+console.log(oddsAndEvens);
